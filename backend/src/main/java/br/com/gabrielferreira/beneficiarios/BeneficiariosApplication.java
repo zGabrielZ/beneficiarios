c69ac823688817b1d819ff615cc6b698f6214247
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2024-02-18 11:01:05 -0300

    Inicio projeto

---
+++ backend/src/main/java/br/com/gabrielferreira/beneficiarios/BeneficiariosApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielferreira.beneficiarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficiariosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/gabrielferreira/beneficiarios/BeneficiariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielferreira.beneficiarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeneficiariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
